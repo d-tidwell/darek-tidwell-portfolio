cb8eaa0f8fb616123addbd2c79b4c7636635adb8
dtidwell <conversations.and.L@gmail.com>
2023-03-25 20:44:31 -0500

    replaced video

--- index_2.js
+++ index_2.js
@@ -3,24 +3,17 @@ function isMobile() {
   }
   var myDIV = document.getElementById("myDiv");
   if (isMobile()) {
-    myDIV.innerHTML = ` <h1>Sorry this page is only available on desktop</h1>`;
+    fetch('2d_html.html')
+        .then(response => response.text())
+        .then(data => {
+        myDIV.innerHTML = data;
+    })
+        .catch(error => console.error(error));
   } else {
-   myDIV.innerHTML = `<div class="progress-bar-container">
-   <label for="progress-bar">Dialing in...</label>
-   <progress id="progress-bar" value="0" max="100"></progress>
-    </div>
- 
-   <video id="video" muted="true" loop preload="auto"  autoplay="true" width="1920" height="1080" src="video/floor_video.mp4" playbackRate="10" playsinline webkit-playsinline ></video>
-   <script>
-     let count = 0;
-     document.addEventListener('mousemove', () => {
-       if (count < 1){
-         document.getElementById('video').play();
-         count += 1;
-         console.log(count, "video play button");
-       }
-     });
-   </script>
-   
-   <canvas class="webgl"></canvas>`
+    fetch('3d_html.html')
+    .then(response => response.text())
+    .then(data => {
+        myDIV.innerHTML = data;
+    })
+    .catch(error => console.error(error));
   }
